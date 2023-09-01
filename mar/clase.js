862845be928ca79afd8e32c53a97d969c5e75277
martinasemper <136343432+martinasemper@users.noreply.github.com>
2023-09-01 08:51:01 -0300

    Add files via upload

---
+++ mar/clase.js
@@ -0,0 +1,51 @@
+
+
+  //1)
+function cuadrado (num)
+{
+   return num * num
+}
+ //2)
+
+ function cubo (num)
+ {
+    return num * num * num
+ }
+
+  //3)
+  
+//A)
+
+ function opercion (x)
+ {
+    cuadrado (x) - x - 10
+ }
+
+ //c)
+ function operacion (x)
+ {
+let rrsultado = (xs * x * x ) + 20
+return resultado 
+ }
+ //d
+function operacio  (x)
+ {
+let resultado = (x / x) + 5 
+return resultado
+ }
+
+ //e
+
+function operacion (x)
+{
+   let resultado = (x * x * x) + (x * x) + 10
+   return resultado 
+
+}
+ 
+// f
+function operacion (x)
+{
+   let resultado = (x*10) *5
+   return resultado 
+}
